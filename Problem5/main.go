8a2db59fbc603bb0bb22fa7d7965d8a45a7de13b
Ardietheparty <94144298+Ardietheparty@users.noreply.github.com>
2021-12-10 16:52:46 -0700

    Best methode

--- Problem5/main.go
+++ Problem5/main.go
@@ -12,19 +12,61 @@ Smallest multiple
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
  */
+
 func main() {
+	MAXIT := 1000000000000
+	//generic way
+	//Brute force
 	timeStart := time.Now()
+	var div []int
+	for i := 2; i <= 20; i++ {
+		div = append(div,i)
+	}
+	fmt.Println(div)
+	for i := 2; i < MAXIT; i++ {
+		test := mult(i,div)
+		if test {
+			fmt.Println(i)
+			i = MAXIT+100
+		}
+	}
+	fmt.Println(time.Since(timeStart))
+	//Elgentish methode
+	//Note that 2520 is divisable by 1,10 so we only need to check 11,20
+	//If we only check multiples of 2520
+	timeStart1 := time.Now()
 	divsors := []int{11,12,13,14,15,16,17,18,19,20}
-	for i:=2520; i<= 100000000000; i+=2520 {
+	for i:=2520; i < MAXIT; i+=2520 {
 		test := mult(i,divsors)
 		if test {
 			fmt.Println(i)
-			i=100000000000*2
+			i = MAXIT+100
 		}
 	}
+	fmt.Println(time.Since(timeStart1))
+	//To scale
+	//Best way
+	// Find the smallest multiple of 1,2 then 1,2,3 then 1,2,3,4 etc
+	timeStart2 := time.Now()
+	biggestDivisor := 21
+	hold := 2
+	n := 0
+	for i := 3; i < biggestDivisor+1; i++ {
+		n = hold
+		test := 0
+		for test == 0 {
+			if n%i == 0 {
+				//fmt.Println(n)
+				hold = n
+				test =1
+			} else {
+				n += hold
+			}
 
-
-	fmt.Println(time.Since(timeStart))
+		}
+	}
+	fmt.Println(n)
+	fmt.Println(time.Since(timeStart2))
 }
 func mult(i int, d []int) bool {
 	for n := 0; n < len(d); n++ {
